54bd301970c4abf6acdc2f0d888ed218ae5e3db5
Fitz Ryland <fitz@pixelspoke.com>
2015-02-27 14:30:54 -0800

    some basic rearanging of html stuff

--- wp-content/themes/pixelspoke-startertheme/page.php
+++ wp-content/themes/pixelspoke-startertheme/page.php
@@ -17,12 +17,29 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <header class="entry-header">
+            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+          </header><!-- .entry-header -->
+
+          <div class="entry-content">
+            <?php the_content(); ?>
+            <?php
+              wp_link_pages( array(
+                'before' => '<div class="page-links">' . __( 'Pages:', 'pixelspoke-startertheme' ),
+                'after'  => '</div>',
+              ) );
+            ?>
+          </div><!-- .entry-content -->
+
+          <footer class="entry-footer">
+            <?php edit_post_link( __( 'Edit', 'pixelspoke-startertheme' ), '<span class="edit-link">', '</span>' ); ?>
+          </footer><!-- .entry-footer -->
+        </article><!-- #post-## -->
 
 			<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
