b888ccbdf382dbe491601af41965bf4b539d1eae
Fitzhugh Ryland <fitzryland@Fitzhughs-MacBook-Pro.local>
2015-09-27 16:17:23 -0700

    added basic plugins, google analytics fonts, and some basic styles

--- wp-content/themes/fitztogether/header.php
+++ wp-content/themes/fitztogether/header.php
@@ -17,6 +17,19 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
+<script type="text/javascript">
+  WebFontConfig = {
+    google: { families: [ 'Raleway:600:latin', 'Roboto:300,500:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); </script>
 </head>
 
 <body <?php body_class(); ?>>
